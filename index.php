e67659d22c95f06926826d439e83cf2e137b8cb8
saadrds <saadrds@gmail.com>
2021-09-24 09:47:00 +0200

    index php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenue à votre Espace Client</h1>
+    <h2>Mes informations personelles : </h2>
+    <div>
+        <ul>
+        <li><p>Nom :</p><p id="nom"></p></li>
+        <li><p>Prénom :</p><p id="prenom"></p></li>
+        <li><p>RIB :</p><p id="rib"></p></li>
+        <li><p>Date de Naissance :</p><p id="naissance"></p></li>
+        <li><p>Civilité:</p><p id="civilite"></p></li>
+        <li><p>Adresse Mail</p><p id="mail"></p></li>
+        <li><p>Adresse :</p><p id="adresse"></p></li>
+        <ul>
+
+    </div>
+</body>
+</html>
